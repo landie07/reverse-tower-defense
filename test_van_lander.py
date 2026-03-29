6327a5f7fa999fb5b3f8ab43c9f39ebb6f08d144
landie07 <lander.vaninbroukx@vub.be>
2026-03-29 21:43:39 +0200

    Update test_van_lander.py

--- test_van_lander.py
+++ test_van_lander.py
@@ -1,4 +1,5 @@
 import troop_classes
+import gebouwen
 import pygame
 import numpy as np
 import random
@@ -8,9 +9,21 @@ screen_height = 500
 screen = pygame.display.set_mode([screen_width, screen_height])
 clock = pygame.time.Clock()
 small_troop1 = troop_classes.small_troop(50, 50, (50, 50), 50)
+building_1 = gebouwen.
 
-
-
+def make_grid(surface:pygame.Surface,color:tuple,height_block:int,width_block:int,height_screen:int,width_screen:int):
+    
+    number_of_rows = height_screen // height_block # // range heeft int nodig
+    number_of_colomns = width_screen // width_block
+    for row in range(number_of_rows):
+        for colomn in range(number_of_colomns):
+            x = colomn*width_block
+            y = row * height_block
+          
+            block = pygame.Rect(x, y,width_block,height_block )
+ 
+            pygame.draw.rect(surface, color, block)
+            
 def create_maze(dim, saturation):
     """
     0 is open plaats
@@ -41,8 +54,11 @@ running = True
 while running:
     clock.tick(20)
     screen.fill(color=(0,0,0))
-   # small_troop1.draw_troop(screen, (255,255,255), (0, 0), radius = 50, alive = True)
-    maze = create_maze(24, 20)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    grid = make_grid(screen, (0,0,0), 50, 65, 500, 650)
+    small_troop1.draw_troop(screen, (255,255,255), (0, 0), radius = 50, alive = True)
     small_troop1.move((5, 5), 5, )
    # small_troop1.find_nearest_building()
     pygame.display.flip()
